b71d5680955cd99fb02b8fa05231c66b817c3186
KRISHNA <miselfkrishna@gmail.com>
2025-10-09 22:25:37 +0530

    9-10-2025 prac

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, reset } from "./counterSlice";
+import React from "react"; 
+import Signup from "./Signup";
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h2>Counter: {count}</h2>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>−</button>
-      <button onClick={() => dispatch(reset())}>Reset</button>
+    <div>
+      <Signup />
     </div>
   );
 }
